184ecef6dd8ee7f9e301777624fa7b88e0a1897f
Ajay Vataliya <vatliyaa@gmail.com>
2024-01-02 12:46:27 +0530

    Add server testing api

--- src/route.js
+++ src/route.js
@@ -2,6 +2,9 @@ const express = require("express")
 const v1Routes = require("./v1/routes")
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({'hello' : "server started"});
+})
 router.use('/api/v1', v1Routes);
 
 module.exports = router;
